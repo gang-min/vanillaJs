1197e0c37ff25fd8343922e0154966b106659c0d
gang-min <rudals8920@naver.com>
2021-09-24 18:41:50 +0900

    Memory Cards App

---
+++ memory-cards/README.md
@@ -0,0 +1,29 @@
+# Memory Cards
+
+- Flash card app for learning. Display, add and remove memory cards with questions and answers
+
+## `✏️ Project Specifications`
+
+- Create flip cards using CSS
+
+  ![Memory Cards](/image/memoryCards.png)
+
+- Create "Add new card" overlay with form
+
+  ![Memory Cards](/image/addNewCard.png)
+
+- Display question cards and flip for answer
+
+  ![Memory Cards](/image/flip.gif)
+
+- View prev and next cards
+
+  ![Memory Cards](/image/slide2.gif)
+
+- Add new cards to local storage
+
+  ![Memory Cards](/image/localStorage.png)
+
+- Clear all cards from local storage
+
+  ![Memory Cards](/image/localStorage2.png)
--- memory-cards/main.js
+++ memory-cards/main.js
@@ -1,10 +1,10 @@
 const cardsContainer = document.querySelector(".cards-container");
 const cardsWrapper = document.querySelector(".cards");
-const front = document.querySelectorAll(".inner-card__front");
-const back = document.querySelectorAll(".inner-card__back");
+let front = document.querySelectorAll(".inner-card__front");
+let back = document.querySelectorAll(".inner-card__back");
 
-const cards = document.querySelectorAll(".card");
-const cardsCount = cards.length;
+let cards = document.querySelectorAll(".card");
+let cardsCount = cards.length;
 let currentIndex = 0;
 
 const prevBtn = document.querySelector(".nav__prev");
@@ -12,12 +12,36 @@ const nextBtn = document.querySelector(".nav__next");
 
 const navCurrent = document.querySelector(".nav__current");
 
-// 카드 뒤집기
-cardsContainer.addEventListener("click", () => {
+const addBtn = document.querySelector(".addBtn");
+const addContainer = document.querySelector(".add-container");
+const closeBtn = document.querySelector(".closeBtn");
+
+const question = document.querySelector("#question");
+const answer = document.querySelector("#answer");
+const addCard = document.querySelector(".add-card");
+
+const clearBtn = document.querySelector(".clear__btn");
+
+let questionArr = [];
+let answerArr = [];
+
+function flip() {
   cards[currentIndex].classList.toggle("flip");
+}
+
+function frontShowAnswer() {
   front[currentIndex].classList.toggle("show-answer");
+}
+
+function backShowAnswer() {
   back[currentIndex].classList.toggle("show-answer");
-});
+}
+
+function cardFlip() {
+  flip();
+  frontShowAnswer();
+  backShowAnswer();
+}
 
 // cards position 미리 잡아 놓기
 function cardPosition() {
@@ -29,6 +53,9 @@ cardPosition();
 
 // navCurrent 변경
 function navCurrentInnerText() {
+  if (!cards[0]) {
+    currentIndex -= 1;
+  }
   navCurrent.innerText = `${currentIndex + 1} / ${cardsCount}`;
 }
 
@@ -59,10 +86,6 @@ prevBtn.addEventListener("click", () => {
 });
 
 // add new card
-const addBtn = document.querySelector(".addBtn");
-const addContainer = document.querySelector(".add-container");
-const closeBtn = document.querySelector(".closeBtn");
-
 addBtn.addEventListener("click", () => {
   addContainer.classList.add("show");
 });
@@ -71,13 +94,15 @@ closeBtn.addEventListener("click", () => {
   addContainer.classList.remove("show");
 });
 
-const question = document.querySelector("#question");
-const answer = document.querySelector("#answer");
-const addCard = document.querySelector(".add-card");
-
 addCard.addEventListener("click", () => {
   const questionValue = question.value;
   const answerValue = answer.value;
+
+  questionArr.push(questionValue);
+  answerArr.push(answerValue);
+
+  localStorage.setItem("question", JSON.stringify(questionArr));
+  localStorage.setItem("answer", JSON.stringify(answerArr));
 
   cardsWrapper.innerHTML += `
     <div class="card">
@@ -85,7 +110,64 @@ addCard.addEventListener("click", () => {
       <div class="inner-card__back">${answerValue}</div>
     </div>
   `;
+
+  front = document.querySelectorAll(".inner-card__front");
+  back = document.querySelectorAll(".inner-card__back");
+  cards = document.querySelectorAll(".card");
+
+  cardsCount = cards.length;
+
+  cardPosition();
+  navCurrentInnerText();
+  goToSlide(0);
+
+  //카드 뒤집기
+  cardsContainer.addEventListener("click", cardFlip);
+
   question.value = "";
   answer.value = "";
   addContainer.classList.remove("show");
+});
+
+// localStorage에 저장된게 있다면 보여주기
+if (localStorage.getItem("question")) {
+  questionArr = JSON.parse(localStorage.getItem("question"));
+  answerArr = JSON.parse(localStorage.getItem("answer"));
+  console.log(questionArr);
+  console.log(answerArr);
+
+  for (let i = 0; i < questionArr.length; i++) {
+    cardsWrapper.innerHTML += `
+    <div class="card">
+      <div class="inner-card__front">${questionArr[i]}</div>
+      <div class="inner-card__back">${answerArr[i]}</div>
+    </div>
+  `;
+  }
+  cards = document.querySelectorAll(".card");
+  cardsCount = cards.length;
+  cardPosition();
+
+  front = document.querySelectorAll(".inner-card__front");
+  back = document.querySelectorAll(".inner-card__back");
+  currentIndex += 1;
+  navCurrentInnerText();
+
+  // 카드뒤집기
+  cardsContainer.addEventListener("click", cardFlip);
+}
+
+// localStorage 비우기
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  questionArr = [];
+  answerArr = [];
+
+  cardsWrapper.innerHTML = "";
+  currentIndex = 0;
+  cards = document.querySelectorAll(".card");
+  cardsCount = cards.length;
+  navCurrentInnerText();
+  // 카드가 없다면 작동x
+  cardsContainer.removeEventListener("click", cardFlip);
 });
